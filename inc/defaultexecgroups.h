3005cafacd533fe20d8e319f1a431e0d52bfd9c8
ZaoLahma <thyberg.jan@gmail.com>
2018-04-03 19:18:21 +0200

    Fixed missing include

--- inc/defaultexecgroups.h
+++ inc/defaultexecgroups.h
@@ -7,6 +7,7 @@
 
 #ifndef INC_DEFAULTEXECGROUPS_H_
 #define INC_DEFAULTEXECGROUPS_H_
+#include <cstdint>
 
 static const uint32_t DEFAULT_EXEC_GROUP_ID = 0;
 static const uint32_t LOGGING_EXEC_GROUP_ID = 1;
